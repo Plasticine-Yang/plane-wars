665125692b83e1520b0e46cadc77650796522086
plasticine9750 <975036719@qq.com>
2022-06-09 19:37:52 +0800

    workflow(bullet): implement Bullet class

---
+++ src/pixi-game/bullet/index.test.ts
@@ -0,0 +1,22 @@
+import { getElementCenterInContainer } from '@/utils';
+import { describe, it, expect } from 'vitest';
+import { BULLET_HEIGHT, BULLET_WIDTH } from '../constants';
+import { Bullet } from './Bullet';
+
+describe('Bullet', () => {
+  const { x: bulletInitX, y: bulletInitY } = getElementCenterInContainer(
+    100,
+    100,
+    BULLET_WIDTH,
+    BULLET_HEIGHT
+  );
+
+  const createBullet = () => {
+    return new Bullet(bulletInitX, bulletInitY, 5);
+  };
+  it('move', () => {
+    const bullet = createBullet();
+    bullet.move();
+    expect(bullet.y).toBe(bulletInitY - bullet.speed);
+  });
+});
