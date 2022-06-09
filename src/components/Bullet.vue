a0793f9946503ed0fb84e2fd30e56ce763183493
plasticine9750 <975036719@qq.com>
2022-06-09 19:37:14 +0800

    workflow(bullet): bullet ui logic

---
+++ src/components/Bullet.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import bulletImg from '@/assets/bullets/player-bullet.png';
+
+defineProps<{ x: number; y: number }>();
+</script>
+
+<template>
+  <Container>
+    <Sprite :texture="bulletImg" :x="x" :y="y" />
+  </Container>
+</template>
