c9bc32b2ac7f483a6f0243c67ef6848902b98998
plasticine9750 <975036719@qq.com>
2022-06-09 11:14:56 +0800

    build: path alias @ to src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import { resolve } from 'path';
+
+const root = process.cwd();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      '@': resolve(root, 'src'),
+    },
+  },
+  plugins: [vue()],
+});
