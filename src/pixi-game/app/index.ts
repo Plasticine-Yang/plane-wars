602dc73db2e8630bd6c2e81f885f4f79659a684e
plasticine9750 <975036719@qq.com>
2022-06-09 19:16:54 +0800

    workflow(app): init app size and background color

--- src/pixi-game/app/index.ts
+++ src/pixi-game/app/index.ts
@@ -1,5 +1,10 @@
 import { Application } from 'pixi.js';
+import { APP_BACKGROUND_COLOR } from '../constants';
 
-export const pixiApp = new Application();
+export const pixiApp = new Application({
+  width: document.documentElement.clientWidth,
+  height: document.documentElement.clientHeight,
+  backgroundColor: APP_BACKGROUND_COLOR,
+});
 
 document.body.appendChild(pixiApp.view);
