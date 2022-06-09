868fede5f721bef61912f6cdf69c2092b73981a9
plasticine9750 <975036719@qq.com>
2022-06-09 19:14:22 +0800

    workflow: define some constants

---
+++ src/pixi-game/constants.ts
@@ -0,0 +1,8 @@
+// app constants
+export const APP_BACKGROUND_COLOR = 0x06283d;
+
+// plane constants
+export { PLANE_HEIGHT, PLANE_WIDTH } from './plane';
+
+// bullet constants
+export { BULLET_HEIGHT, BULLET_WIDTH } from './bullet';
