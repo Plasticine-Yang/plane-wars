12ba2d130e57a9ee268e30a73d8e7481841b391a
plasticine9750 <975036719@qq.com>
2022-06-09 19:17:04 +0800

    style: basic style of app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from './runtime-pixi';
 import { pixiApp } from './pixi-game';
 import App from './App.vue';
+import '@/style/index.scss';
 
 createApp(App).mount(pixiApp.stage);
