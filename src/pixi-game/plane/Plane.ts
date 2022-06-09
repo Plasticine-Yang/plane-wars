6abc94bd827134ef689c39066a69d15d78e3019f
plasticine9750 <975036719@qq.com>
2022-06-09 20:02:27 +0800

    workflow(plane): fire continueously

--- src/pixi-game/plane/Plane.ts
+++ src/pixi-game/plane/Plane.ts
@@ -5,6 +5,8 @@ export const PLANE_HEIGHT = 98;
 
 interface PlaneOptions {
   bulletMoveSpeed: number;
+  /** 开炮的频率 每 fireFreq 毫秒就开 1 炮 */
+  fireFreq: number;
 }
 
 export class Plane {
@@ -13,12 +15,14 @@ export class Plane {
   speed: number;
   bullets: Bullet[] = [];
   bulletMoveSpeed: number;
+  fireFreq: number;
 
   constructor(x: number, y: number, speed: number, options?: PlaneOptions) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.bulletMoveSpeed = options?.bulletMoveSpeed ?? 5;
+    this.fireFreq = options?.fireFreq ?? 300;
   }
 
   moveUp() {
@@ -49,5 +53,15 @@ export class Plane {
    */
   moveBullets() {
     this.bullets.forEach((bullet) => bullet.move());
+  }
+
+  /**
+   * @description 飞机运行时的逻辑
+   */
+  run() {
+    // 不断开炮
+    setInterval(() => {
+      this.fire();
+    }, this.fireFreq);
   }
 }
