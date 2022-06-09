09371654a2dea6cd2f403123a4018e2b77d92192
plasticine9750 <975036719@qq.com>
2022-06-09 19:38:31 +0800

    workflow(plane): fire and moveBullets

--- src/pixi-game/plane/Plane.ts
+++ src/pixi-game/plane/Plane.ts
@@ -1,15 +1,24 @@
+import { Bullet } from '../bullet';
+
 export const PLANE_WIDTH = 110;
 export const PLANE_HEIGHT = 98;
+
+interface PlaneOptions {
+  bulletMoveSpeed: number;
+}
 
 export class Plane {
   x: number;
   y: number;
   speed: number;
+  bullets: Bullet[] = [];
+  bulletMoveSpeed: number;
 
-  constructor(x: number, y: number, speed: number) {
+  constructor(x: number, y: number, speed: number, options?: PlaneOptions) {
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.bulletMoveSpeed = options?.bulletMoveSpeed ?? 5;
   }
 
   moveUp() {
@@ -26,5 +35,19 @@ export class Plane {
 
   moveRight() {
     this.x += this.speed;
+  }
+
+  /**
+   * @description 开火 -- 生成子弹
+   */
+  fire() {
+    this.bullets.push(new Bullet(this.x, this.y, this.bulletMoveSpeed));
+  }
+
+  /**
+   * @description 让子弹移动
+   */
+  moveBullets() {
+    this.bullets.forEach((bullet) => bullet.move());
   }
 }
--- src/pixi-game/plane/index.test.ts
+++ src/pixi-game/plane/index.test.ts
@@ -33,4 +33,25 @@ describe('Plane', () => {
     plane.moveRight();
     expect(plane.x).toBe(30);
   });
+
+  it('fire', () => {
+    const plane = createPlane();
+    plane.fire();
+    expect(plane.bullets.length).toBe(1);
+  });
+
+  it('moveBullets', () => {
+    const plane = createPlane();
+
+    // 开 10 炮 -- 生成 10 个子弹
+    for (let i = 0; i < 10; i++) plane.fire();
+
+    // 让子弹动起来
+    plane.moveBullets();
+
+    for (const bullet of plane.bullets) {
+      // 子弹如果有移动的话 y 坐标肯定不会和飞机的初始 y 坐标一样
+      expect(bullet.y).not.toBe(plane.y);
+    }
+  });
 });
